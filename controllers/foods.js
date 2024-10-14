57f1b7455c4ed1b28fc2789914dde174e44594ad
joshuanikolas <nikolassnchez@icloud.com>
2024-10-13 21:19:24 -0500

    index page works

--- controllers/foods.js
+++ controllers/foods.js
@@ -26,7 +26,7 @@ router.get('/', async (req, res) => {
     try {
       const currentUser = await User.findById(req.session.user._id);
       res.render('foods/index.ejs', {
-        foods: currentUser.pantry
+        pantry: currentUser.pantry
       })
     } catch (error) {
       console.log(error)
