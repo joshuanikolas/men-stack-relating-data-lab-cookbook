b04748a838db2cbe15ebbbe78377be87edbe73a6
joshuanikolas <nikolassnchez@icloud.com>
2024-10-13 21:49:38 -0500

    edit route works

--- controllers/foods.js
+++ controllers/foods.js
@@ -37,7 +37,7 @@ router.get('/', async (req, res) => {
 router.delete('/:foodId', async (req, res) => {
     try {
       const currentUser = await User.findById(req.session.user._id);
-      currentUser.pantry.id(req.params.foodsId).deleteOne();
+      currentUser.pantry.id(req.params.foodId).deleteOne();
       await currentUser.save();
       res.redirect(`/users/${currentUser._id}/foods`);
     } catch (error) {
@@ -49,7 +49,7 @@ router.delete('/:foodId', async (req, res) => {
 router.get('/:foodId/edit', async (req, res) => {
     try {
       const currentUser = await User.findById(req.session.user._id);
-      const food = currentUser.food.id(req.params.foodId);
+      const food = currentUser.pantry.id(req.params.foodId);
       res.render('foods/edit.ejs', {food: food,});
     } catch (error) {
       console.log(error);
